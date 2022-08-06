6c0d903bf0ca45fd0df18807814ba1c697ea5788
Denis <denis.tolmachev@gmail.com>
2022-08-06 19:32:00 +0300

    create store

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import { App } from 'App';
 import './index.css';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'theme';
+import store from 'store/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
