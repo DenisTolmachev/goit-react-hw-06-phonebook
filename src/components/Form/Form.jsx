2a45b3f28594f74048188a21cca65534589ac847
Denis <denis.tolmachev@gmail.com>
2022-08-13 16:17:43 +0300

    correct yup pattern

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -12,9 +12,31 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getItems } from 'store/contacts/contacts';
 import { addContact } from 'store/contacts/contacts';
 
+const phoneRegex = RegExp(
+  /\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/
+);
+const nameRegex = RegExp(
+  /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/
+);
+
 const mySchema = yup.object().shape({
-  name: yup.string().min(2).required('Name is required'),
-  number: yup.string().length(7).required('Number is required'),
+  name: yup
+    .string()
+    .min(2)
+    .matches(
+      nameRegex,
+      "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+    )
+    .required('Name is required'),
+
+  number: yup
+    .string()
+    .length(7)
+    .matches(
+      phoneRegex,
+      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+    )
+    .required('Number is required'),
 });
 
 export const ContactForm = () => {
@@ -27,11 +49,11 @@ export const ContactForm = () => {
     return normalizedNumber;
   };
 
-  const contactsCheck = (name) => {
-    const result = name.name.toLowerCase()
-    const data = items.find(item => item.name.toLowerCase().includes(result))
+  const contactsCheck = name => {
+    const result = name.name.toLowerCase();
+    const data = items.find(item => item.name.toLowerCase().includes(result));
     return data;
-  }
+  };
 
   const handleSubmit = (values, { resetForm }) => {
     const newName = {
@@ -39,13 +61,13 @@ export const ContactForm = () => {
       name: values.name,
       number: normalizedNumber(values.number),
     };
-    
+
     if (contactsCheck(newName)) {
-      alert(`${newName.name} is already in contacts`)
+      alert(`${newName.name} is already in contacts`);
     } else {
       dispatch(addContact(newName));
     }
-    
+
     resetForm();
   };
 
@@ -62,8 +84,6 @@ export const ContactForm = () => {
             <ContactFormInput
               type="text"
               name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               placeholder="Rosie Simpson"
               value={props.values.name}
               onChange={props.handleChange}
@@ -78,8 +98,6 @@ export const ContactForm = () => {
             <ContactFormInput
               type="tel"
               name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               placeholder="Enter 7 digits"
               value={props.values.number}
               onChange={props.handleChange}
